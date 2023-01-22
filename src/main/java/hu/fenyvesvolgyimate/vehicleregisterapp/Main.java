bc1f8b5e78e4c4e54ad708664b8e4c7fb0e49fa2
fenyvesvolgyimate <fenyves96@gmail.com>
2023-01-22 19:37:07 +0100

    refactor packages

---
+++ src/main/java/hu/fenyvesvolgyimate/vehicleregisterapp/Main.java
@@ -0,0 +1,22 @@
+package hu.fenyvesvolgyimate.vehicleregisterapp;
+
+import hu.fenyvesvolgyimate.vehicleregisterapp.controller.VehicleController;
+import hu.fenyvesvolgyimate.vehicleregisterapp.dao.FileVehicleRepository;
+import hu.fenyvesvolgyimate.vehicleregisterapp.dao.VehicleRepository;
+import hu.fenyvesvolgyimate.vehicleregisterapp.interactor.JsonVehicleRegister;
+import hu.fenyvesvolgyimate.vehicleregisterapp.interactor.VehicleRegister;
+import hu.fenyvesvolgyimate.vehicleregisterapp.controller.VehicleConsoleController;
+import hu.fenyvesvolgyimate.vehicleregisterapp.presenter.VehicleConsolePresenter;
+import hu.fenyvesvolgyimate.vehicleregisterapp.presenter.VehicleUserInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        VehicleRepository vehicleStorage = new FileVehicleRepository();
+        VehicleUserInterface vehicleConsolePresenter = new VehicleConsolePresenter();
+
+        VehicleRegister vehicleRegister = new JsonVehicleRegister(vehicleStorage, vehicleConsolePresenter);
+        VehicleController vehicleController = new VehicleConsoleController(vehicleRegister);
+
+        vehicleController.start();
+    }
+}
