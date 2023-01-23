74e7b0d8584c0917f83389e11d761d7b430e3be4
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2023-01-23 20:36:36 +0100

    separate VehicleWriter from VehicleReader

--- src/main/java/hu/fenyvesvolgyimate/vehicleregisterapp/Main.java
+++ src/main/java/hu/fenyvesvolgyimate/vehicleregisterapp/Main.java
@@ -3,7 +3,6 @@ package hu.fenyvesvolgyimate.vehicleregisterapp;
 import hu.fenyvesvolgyimate.vehicleregisterapp.controller.VehicleController;
 import hu.fenyvesvolgyimate.vehicleregisterapp.storage.FileVehicleRepository;
 import hu.fenyvesvolgyimate.vehicleregisterapp.storage.VehicleRepository;
-import hu.fenyvesvolgyimate.vehicleregisterapp.interactor.JsonVehicleRegister;
 import hu.fenyvesvolgyimate.vehicleregisterapp.interactor.VehicleRegister;
 import hu.fenyvesvolgyimate.vehicleregisterapp.controller.VehicleConsoleController;
 import hu.fenyvesvolgyimate.vehicleregisterapp.presenter.VehiclePresenterImpl;
@@ -15,8 +14,8 @@ public class Main {
         VehicleRepository vehicleStorage = new FileVehicleRepository();
         VehiclePresenter vehicleConsolePresenter = new VehiclePresenterImpl();
 
-        VehicleRegister vehicleRegister = new JsonVehicleRegister(vehicleStorage, vehicleConsolePresenter);
-        VehicleController vehicleController = new VehicleConsoleController(vehicleRegister);
+        VehicleRegister vehicleRegister = new VehicleRegister(vehicleStorage, vehicleConsolePresenter);
+        VehicleController vehicleController = new VehicleConsoleController(vehicleRegister, vehicleRegister);
 
         VehicleConsoleView vehicleConsoleView = new VehicleConsoleView(vehicleController);
         vehicleConsoleView.start();
