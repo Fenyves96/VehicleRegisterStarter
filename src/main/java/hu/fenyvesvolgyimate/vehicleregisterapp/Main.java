13e778748ac4dd3e7cb96acc4b08b051fb1cbd31
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2023-01-23 14:27:32 +0100

    team rules: Presenter, presenterImpl

--- src/main/java/hu/fenyvesvolgyimate/vehicleregisterapp/Main.java
+++ src/main/java/hu/fenyvesvolgyimate/vehicleregisterapp/Main.java
@@ -6,14 +6,14 @@ import hu.fenyvesvolgyimate.vehicleregisterapp.storage.VehicleRepository;
 import hu.fenyvesvolgyimate.vehicleregisterapp.interactor.JsonVehicleRegister;
 import hu.fenyvesvolgyimate.vehicleregisterapp.interactor.VehicleRegister;
 import hu.fenyvesvolgyimate.vehicleregisterapp.controller.VehicleConsoleController;
-import hu.fenyvesvolgyimate.vehicleregisterapp.presenter.VehicleConsolePresenter;
-import hu.fenyvesvolgyimate.vehicleregisterapp.presenter.VehicleUserInterface;
+import hu.fenyvesvolgyimate.vehicleregisterapp.presenter.VehiclePresenterImpl;
+import hu.fenyvesvolgyimate.vehicleregisterapp.presenter.VehiclePresenter;
 import hu.fenyvesvolgyimate.vehicleregisterapp.view.VehicleConsoleView;
 
 public class Main {
     public static void main(String[] args) {
         VehicleRepository vehicleStorage = new FileVehicleRepository();
-        VehicleUserInterface vehicleConsolePresenter = new VehicleConsolePresenter();
+        VehiclePresenter vehicleConsolePresenter = new VehiclePresenterImpl();
 
         VehicleRegister vehicleRegister = new JsonVehicleRegister(vehicleStorage, vehicleConsolePresenter);
         VehicleController vehicleController = new VehicleConsoleController(vehicleRegister);
